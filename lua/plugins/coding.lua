f0457b6c2c420fd0ed3b9bbcc5857bd793176c08
GP4cK <gautier.bayzelon@gmail.com>
2025-09-19 16:01:31 +0800

    setups nvim-treesitter-context

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -22,4 +22,16 @@ return {
     "mg979/vim-visual-multi",
     branch = "master",
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context", -- [c to jump to context
+    event = "VeryLazy",
+    opts = {
+      enable = true, -- enable this plugin (true/false)
+      max_lines = 3, -- how many lines the context window should show (0 = unlimited)
+      trim_scope = "outer", -- which context lines to discard if max_lines is exceeded
+      mode = "cursor", -- line used to calculate context (cursor|topline)
+      multiline_threshold = 20, -- max number of lines for a single context
+      zindex = 20, -- priority of the context window
+    },
+  },
 }
