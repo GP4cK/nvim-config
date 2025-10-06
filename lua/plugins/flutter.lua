d00b0a4d7ca84da7d7907416b5d5aaa7ee0a7ce5
GP4cK <gautier.bayzelon@gmail.com>
2025-10-06 20:44:09 +0800

    adds exrc to debug flutter

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,33 +1,50 @@
 return {
-  "nvim-flutter/flutter-tools.nvim",
-  lazy = false,
-  event = "BufReadPre *.dart",
-  dependencies = "nvim-lua/plenary.nvim",
-  config = function()
-    require("flutter-tools").setup({
-      lsp = {
-        color = {
-          enabled = true,
-        },
-        on_attach = function(client)
-          client.server_capabilities.inlayHintProvider = false
-        end,
-        settings = {
-          lineLength = 160,
-          analysisExcludedFolders = {
-            ".dart_tool",
-            "/Users/gautier/.pub-cache/",
-            "/Users/gautier/fvm/",
+  {
+    "jedrzejboczar/exrc.nvim",
+    dependencies = { "neovim/nvim-lspconfig" }, -- (optional)
+    config = true,
+    opts = { --[[ your config ]]
+    },
+  },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    event = "BufReadPre *.dart",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("flutter-tools").setup({
+        lsp = {
+          color = {
+            enabled = true,
+          },
+          on_attach = function(client)
+            client.server_capabilities.inlayHintProvider = false
+          end,
+          settings = {
+            lineLength = 160,
+            analysisExcludedFolders = {
+              ".dart_tool",
+              "/Users/gautier/.pub-cache/",
+              "/Users/gautier/fvm/",
+            },
           },
         },
-      },
-      fmv = true,
-      default_run_args = { flutter = "--no-pub" },
-      dev_tools = {
-        autostart = false,
-        auto_open_browser = false,
-      },
-    })
-    require("telescope").load_extension("flutter")
-  end,
+        fmv = true,
+        debugger = {
+          enabled = true,
+        },
+        decorations = {
+          statusline = {
+            device = true,
+          },
+        },
+        default_run_args = { flutter = "--no-pub" },
+        dev_tools = {
+          autostart = false,
+          auto_open_browser = false,
+        },
+      })
+      require("telescope").load_extension("flutter")
+    end,
+  },
 }
