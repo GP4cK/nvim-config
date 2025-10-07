2145b9d237cb232848f2f14077a4d3c82164ad00
GP4cK <gautier.bayzelon@gmail.com>
2025-10-07 21:49:28 +0800

    rebinds <C-x> to clear search highlight to use vim-visual-multi

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-n>", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<C-x>", ":nohl<CR>", { desc = "Clear search highlights" })
 vim.keymap.set("n", "<leader>vb", "viwf(%", { desc = "Select till )" })
 vim.keymap.set("n", "<leader>vB", "viwf{%", { desc = "Select till }" })
 vim.keymap.set({ "i", "v", "c" }, "<C-c>", "<Esc>", { desc = "Exit to normal mode" })
