3e67b3007c64f461f6f57ab2e61940a8f860ed5a
GP4cK <gautier.bayzelon@gmail.com>
2025-10-09 10:00:33 +0800

    ignores dist in neotest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -13,11 +13,25 @@ return {
 
       -- Conditionally register adapters
       if vim.fn.filereadable("jest.config.js") == 1 or vim.fn.filereadable("jest.config.ts") == 1 then
-        table.insert(adapters, require("neotest-jest")({}))
+        table.insert(
+          adapters,
+          require("neotest-jest")({
+            filter_dir = function(_, rel_path)
+              return not rel_path:match("dist") and not rel_path:match("node_modules")
+            end,
+          })
+        )
       end
 
       if vim.fn.filereadable("vitest.config.ts") == 1 or vim.fn.filereadable("vitest.config.js") == 1 then
-        table.insert(adapters, require("neotest-vitest")({}))
+        table.insert(
+          adapters,
+          require("neotest-vitest")({
+            filter_dir = function(_, rel_path)
+              return not rel_path:match("dist") and not rel_path:match("node_modules")
+            end,
+          })
+        )
       end
 
       if vim.fn.filereadable("pubspec.yaml") == 1 then
