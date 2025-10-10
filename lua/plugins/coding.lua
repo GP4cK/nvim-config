7472c6211c56aaaf36e036e8437e49b866017655
GP4cK <gautier.bayzelon@gmail.com>
2025-10-10 14:25:59 +0800

    setups sidekick

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -34,4 +34,27 @@ return {
       zindex = 20, -- priority of the context window
     },
   },
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        mux = {
+          backend = "zellij",
+          enabled = true,
+        },
+      },
+      keymap = {
+        ["<Tab>"] = {
+          "snippet_forward",
+          function() -- sidekick next edit suggestion
+            return require("sidekick").nes_jump_or_apply()
+          end,
+          function() -- if you are using Neovim's native inline completions
+            return vim.lsp.inline_completion.get()
+          end,
+          "fallback",
+        },
+      },
+    },
+  },
 }
