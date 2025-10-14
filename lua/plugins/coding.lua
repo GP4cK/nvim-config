1e476603976750f9713108eeb8bccd03fccdd029
GP4cK <gautier.bayzelon@gmail.com>
2025-10-14 22:02:01 +0800

    view PR in browser

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -57,4 +57,20 @@ return {
       },
     },
   },
+  {
+    "fredrikaverpil/pr.nvim",
+    lazy = true,
+    version = "*",
+    opts = {},
+    keys = {
+      {
+        "<leader>gv",
+        function()
+          require("pr").view()
+        end,
+        desc = "View PR in browser",
+      },
+    },
+    cmd = { "PRView" },
+  },
 }
