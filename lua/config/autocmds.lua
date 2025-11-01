88d61b34fcaa98620197e4d0b861059de2e01c57
GP4cK <gautier.bayzelon@gmail.com>
2025-11-01 10:36:58 +0800

    adds wezterm-move

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+  callback = function()
+    vim.fn.system("wezterm set-user-var IS_NVIM true")
+    local cwd = vim.fn.getcwd()
+    vim.fn.chansend(vim.v.stderr, string.format("\027]7;file://%s%s\027\\", vim.uv.os_gethostname(), cwd))
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = function()
+    vim.fn.system("wezterm set-user-var IS_NVIM false")
+  end,
+})
