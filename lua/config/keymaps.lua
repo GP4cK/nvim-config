8eb8641e091d28c234810b36accfa6778ff355f9
GP4cK <gautier.bayzelon@gmail.com>
2026-09-10 11:11:02 +0800

    removes C-x to clear search highlight. Do it with Escape

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-x>", ":nohl<CR>", { desc = "Clear search highlights" })
 
 vim.keymap.set("n", "<leader>p", function()
   LazyVim.pick("find_files", { root = true })()
