788df0aab7d4c36a1d04ffbb0bdc6b48b08c1122
GP4cK <gautier.bayzelon@gmail.com>
2026-09-11 13:42:48 +0800

    adds octo

---
+++ lua/plugins/octo.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "pwntester/octo.nvim",
+    keys = {
+      { "<leader>gn", "<cmd>Octo notification<cr>", desc = "GitHub Notifications (Octo)" },
+      { "<leader>gC", "<cmd>Octo pr checkout<cr>", desc = "Checkout PR (Octo)" },
+      { "<leader>gR", "<cmd>Octo review<cr>", desc = "Review: start or resume (Octo)" },
+    },
+  },
+}
