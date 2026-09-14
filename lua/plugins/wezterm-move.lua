6de724b690b1e6c8dd278b12d07309e415d0f17c
GP4cK <gautier.bayzelon@gmail.com>
2026-09-14 16:42:12 +0800

    enables window navigation inside herdr

--- lua/plugins/wezterm-move.lua
+++ lua/plugins/wezterm-move.lua
@@ -1,33 +1,43 @@
+local herdr_directions = {
+  h = "left",
+  j = "down",
+  k = "up",
+  l = "right",
+}
+
+local function move(direction)
+  local current_window = vim.fn.winnr()
+  local target_window = vim.fn.winnr(direction)
+
+  if current_window ~= target_window then
+    vim.cmd("wincmd " .. direction)
+    return
+  end
+
+  if vim.env.HERDR_ENV == "1" then
+    vim.system({
+      "herdr",
+      "pane",
+      "focus",
+      "--direction",
+      herdr_directions[direction],
+      "--current",
+    })
+    return
+  end
+
+  require("wezterm-move").move(direction)
+end
+
 return {
   "letieu/wezterm-move.nvim",
-  keys = {
-    {
-      "<C-h>",
+  keys = vim.tbl_map(function(direction)
+    return {
+      "<C-" .. direction .. ">",
       function()
-        require("wezterm-move").move("h")
+        move(direction)
       end,
       mode = { "n", "t" },
-    },
-    {
-      "<C-j>",
-      function()
-        require("wezterm-move").move("j")
-      end,
-      mode = { "n", "t" },
-    },
-    {
-      "<C-k>",
-      function()
-        require("wezterm-move").move("k")
-      end,
-      mode = { "n", "t" },
-    },
-    {
-      "<C-l>",
-      function()
-        require("wezterm-move").move("l")
-      end,
-      mode = { "n", "t" },
-    },
-  },
+    }
+  end, { "h", "j", "k", "l" }),
 }
